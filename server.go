11a43bd13d654c5e8471b4cf04a4d4f761d27689
Eduardo Colina <eacolina@edu.uwaterloo.ca>
2018-07-25 22:45:01 -0400

    Cleaned up server.go

--- server.go
+++ server.go
@@ -16,25 +16,11 @@ var mu sync.Mutex
 func main() {
 	fmt.Println("Starting server...")
 	fmt.Println("Listening on localhost:8080")
-	http.HandleFunc("/", handler)
-	http.HandleFunc("/count", counter)
 	http.HandleFunc("/actions", actions)
 	log.Fatal(http.ListenAndServe("localhost:8080", nil))
 
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	mu.Lock()
-	count++
-	mu.Unlock()
-	fmt.Fprintf(w, "URL.Path = %q", r.URL.Path)
-}
-
-func counter(w http.ResponseWriter, r *http.Request) {
-	mu.Lock()
-	fmt.Fprint(w, "Count: %d", count)
-	mu.Unlock()
-}
 func actions(w http.ResponseWriter, r *http.Request) {
 	reader := r.Body
 	defer reader.Close()
@@ -42,5 +28,4 @@ func actions(w http.ResponseWriter, r *http.Request) {
 	b, _ := ioutil.ReadAll(reader) // create a byte array from the reader stream
 	json.Unmarshal(b,&body) // parse JSON byte array into body
 	w.WriteHeader(http.StatusOK)
-
 }
